91e891127175da8aded40be245bfd85e4fdd21db
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-08-15 21:26:21 -0400

    Complete starter game

--- src/cs/production/MyProject.Content/Content.Sounds.cs
+++ src/cs/production/MyProject.Content/Content.Sounds.cs
@@ -7,12 +7,16 @@ public static partial class Content
     public static class Sounds
     {
         public static SoundEffect Greeting;
-        public static SoundEffect ConstructionComplete;
+        public static SoundEffect GameOver;
+        public static SoundEffect Hit;
+        public static SoundEffect ObjectiveComplete;
 
         internal static void LoadSounds()
         {
             Greeting = SoundEffect.FromFile("assets/sounds/greeting_6_alex.wav");
-            ConstructionComplete = SoundEffect.FromFile("assets/sounds/completion_9_karen.wav");
+            GameOver = SoundEffect.FromFile("assets/sounds/miscellaneous_1_alex.wav");
+            Hit = SoundEffect.FromFile("assets/sounds/BLLTImpt_Hit Marker_07.wav");
+            ObjectiveComplete = SoundEffect.FromFile("assets/sounds/completion_8_alex.wav");
         }
     }   
 }
